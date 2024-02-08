ead608fe13a396d5e103e0f07bcfcea2cbbfee7f
Tom Stesco <tstesco@tenstorrent.com>
2024-02-08 11:16:06 -0500

    change Imagenet 1k sample image to new static url because of change in HF datasets-server API

--- model_demos/cv_demos/mlpmixer/timm_mlpmixer.py
+++ model_demos/cv_demos/mlpmixer/timm_mlpmixer.py
@@ -28,7 +28,7 @@ def run_mlpmixer_timm():
     compiler_cfg.enable_t_streaming = True
 
     # Load data sample
-    url = "https://datasets-server.huggingface.co/assets/imagenet-1k/--/default/train/18/image/image.jpg"
+    url = "https://images.rawpixel.com/image_1300/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3BkMTA2LTA0Ny1jaGltXzEuanBn.jpg"
     image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
     label = "tiger"
 
