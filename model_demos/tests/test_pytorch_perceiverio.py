decdfc96ad2e638e7084f0bd95fd7c6a70aaf0ad
pchandrasekaran <pchandrasekaran@tenstorrent.com>
2024-03-08 06:51:44 -0500

    Update func name for perceiverio

--- model_demos/tests/test_pytorch_perceiverio.py
+++ model_demos/tests/test_pytorch_perceiverio.py
@@ -9,5 +9,5 @@ variants = ["deepmind/vision-perceiver-conv"]
 
 @pytest.mark.parametrize("variant", variants, ids=variants)
 @pytest.mark.perceiverio
-def test_perceiverio_for_image_classification_pytorch(clear_pybuda, variant):
+def test_perceiverio_image_classification_pytorch(clear_pybuda, variant):
     run_perceiverio_for_image_classification_pytorch(variant)
