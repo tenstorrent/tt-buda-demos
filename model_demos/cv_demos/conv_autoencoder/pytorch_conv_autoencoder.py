432e1a0ca7600ac38dfa7f784f6256c6b29b82aa
anirudTT <aramchandran@tenstorrent.com>
2024-03-20 18:13:16 -0400

    lets no try this

--- model_demos/cv_demos/conv_autoencoder/pytorch_conv_autoencoder.py
+++ model_demos/cv_demos/conv_autoencoder/pytorch_conv_autoencoder.py
@@ -1,12 +1,12 @@
 # Convolutional Autoencoder Demo Script
 
 
-import pybuda
-import torch
+
 import torchvision.transforms as transforms
 from datasets import load_dataset
 
-
+import pybuda
+import torch
 class ConvAE(torch.nn.Module):
     def __init__(self):
         super().__init__()
