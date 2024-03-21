a5296013ab87a8d45e310955e784422f50653b70
anirudTT <anirudTT@users.noreply.github.com>
2024-03-21 13:07:23 +0000

    Automated Commit | Applied code formatting and cleanup

--- model_demos/cv_demos/googlenet/pytorch_googlenet_torchhub.py
+++ model_demos/cv_demos/googlenet/pytorch_googlenet_torchhub.py
@@ -1,17 +1,16 @@
 # GoogLeNet Demo Script
 
 
+import urllib
 
+import pybuda
+import requests
+import torch
 from PIL import Image
 from pybuda._C.backend_api import BackendDevice
 from torchvision import models, transforms
 
-import requests
-import torch
 
-import urllib
-
-import pybuda
 def run_googlenet_pytorch():
 
     # Set PyBuda configuration parameters
