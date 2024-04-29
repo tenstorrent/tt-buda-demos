4f715c0e1b97cf930bca5ce13b97aec80bf84b68
anirudTT <aramchandran@tenstorrent.com>
2024-04-29 20:33:40 +0000

    change to test variant passing on alpha releases

--- model_demos/tests/test_pytorch_perceiverio.py
+++ model_demos/tests/test_pytorch_perceiverio.py
@@ -1,13 +1,9 @@
 import pytest
-
-from cv_demos.perceiverio.pytorch_perceiverio_for_image_classification import (
-    run_perceiverio_for_image_classification_pytorch,
-)
+from cv_demos.perceiverio.pytorch_perceiverio import run_perceiverio_pytorch
 
 variants = ["deepmind/vision-perceiver-conv"]
-
 
 @pytest.mark.parametrize("variant", variants, ids=variants)
 @pytest.mark.perceiverio
 def test_perceiverio_image_classification_pytorch(clear_pybuda, variant):
-    run_perceiverio_for_image_classification_pytorch(variant)
+    run_perceiverio_pytorch(variant)
