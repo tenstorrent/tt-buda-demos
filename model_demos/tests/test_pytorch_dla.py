861c1796fdef42504024ea033b97224c58c8fdc3
Ashok Kumar Kannan <akannan@tenstorrent.com>
2024-05-06 16:50:51 +0000

    Add model demos for DLA (Pytorch)

---
+++ model_demos/tests/test_pytorch_dla.py
@@ -0,0 +1,22 @@
+import pytest
+from cv_demos.dla.pytorch_dla import run_dla_pytorch
+
+
+variants = [
+    "dla34",
+    "dla46_c",
+    "dla46x_c",
+    "dla60x_c",
+    "dla60",
+    "dla60x",
+    "dla102",
+    "dla102x",
+    "dla102x2",
+    "dla169",
+]
+
+
+@pytest.mark.dla
+@pytest.mark.parametrize("variant", variants, ids=variants)
+def test_dla_pytorch(clear_pybuda, variant):
+    run_dla_pytorch(variant)
