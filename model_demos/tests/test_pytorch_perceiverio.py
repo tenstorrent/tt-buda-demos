35305eca15056e7738dcaccc0353afcb41dba708
chandrasekaranpradeep <pchandrasekaran@tenstorrent.com>
2024-05-17 07:06:24 +0000

    Add perceiverio fourier and learned image classification model on wh_b0 and gs
    Update the diffusers package from 0.14.0 to 0.27.2

--- model_demos/tests/test_pytorch_perceiverio.py
+++ model_demos/tests/test_pytorch_perceiverio.py
@@ -2,10 +2,15 @@ import pytest
 
 from cv_demos.perceiverio.pytorch_perceiverio import run_perceiverio_pytorch
 
-variants = ["deepmind/vision-perceiver-conv"]
+
+variants = [
+    "deepmind/vision-perceiver-conv",
+    "deepmind/vision-perceiver-fourier",
+    "deepmind/vision-perceiver-learned",
+]
 
 
 @pytest.mark.parametrize("variant", variants, ids=variants)
 @pytest.mark.perceiverio
-def test_perceiverio_image_classification_pytorch(clear_pybuda, variant):
+def test_perceiverio_pytorch(clear_pybuda, variant):
     run_perceiverio_pytorch(variant)
