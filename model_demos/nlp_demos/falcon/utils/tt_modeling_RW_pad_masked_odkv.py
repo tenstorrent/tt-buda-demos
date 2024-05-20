548413ae01e222c294df3d8c339040046e80262e
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-20 15:08:59 -0400

    more lc headers

--- model_demos/nlp_demos/falcon/utils/tt_modeling_RW_pad_masked_odkv.py
+++ model_demos/nlp_demos/falcon/utils/tt_modeling_RW_pad_masked_odkv.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
 # port of models described in RW
 # We use the bloom model as a starting point for these model.
 # Please refer to the bloom models for usage instructions.
