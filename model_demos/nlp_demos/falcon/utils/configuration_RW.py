3d623aeba72f908f95c99d069a4706961fd1c6fc
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-20 15:08:59 -0400

    more lc headers

--- model_demos/nlp_demos/falcon/utils/configuration_RW.py
+++ model_demos/nlp_demos/falcon/utils/configuration_RW.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
 # coding=utf-8
 # Copyright 2022 the Big Science Workshop and HuggingFace Inc. team.  All rights reserved.
 #
--- model_demos/tests/test_tflite_mobilenet_ssd.py
+++ model_demos/tests/test_tflite_mobilenet_ssd.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
 
 from cv_demos.mobilenet_ssd.tflite_mobilenet_v2_ssd_1x1 import run_mobilenetv2_ssd_1x1_tflite
