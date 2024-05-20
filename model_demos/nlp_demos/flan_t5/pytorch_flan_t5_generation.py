1df17a7fab8822b357b5d59520e9c72b0542e3c9
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-20 10:27:03 -0400

    add lc header to more files

--- model_demos/nlp_demos/flan_t5/pytorch_flan_t5_generation.py
+++ model_demos/nlp_demos/flan_t5/pytorch_flan_t5_generation.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
 # FLAN-T5 Demo - Conditional Generation
 
 import os
--- model_demos/nlp_demos/gpt_neo/pytorch_gptneo_causal_lm.py
+++ model_demos/nlp_demos/gpt_neo/pytorch_gptneo_causal_lm.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
 # GPT Neo Demo - CausalLM
 
 import os
