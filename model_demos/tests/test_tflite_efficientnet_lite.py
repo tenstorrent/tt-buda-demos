686e642ad123d67da4a2504b2416e8ab0ac90b06
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-05-20 15:08:59 -0400

    more lc headers

--- model_demos/tests/test_tflite_efficientnet_lite.py
+++ model_demos/tests/test_tflite_efficientnet_lite.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
 
 from cv_demos.efficientnet_lite.tflite_efficientnet_lite0_1x1 import run_efficientnet_lite0_1x1
