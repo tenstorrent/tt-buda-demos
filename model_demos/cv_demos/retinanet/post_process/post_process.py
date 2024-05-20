24dc05883b071366107a79782122b0212098a7cc
kkannan <kkannan@tenstorrent.com>
2024-05-20 17:28:49 +0000

    Add tests for retinanet(pytorch)

---
+++ model_demos/cv_demos/retinanet/post_process/post_process.py
@@ -0,0 +1,190 @@
+"""
+# code apapted from :
+# https://github.com/NVIDIA/retinanet-examples/tree/main/odtk
+
+Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+ * Neither the name of NVIDIA CORPORATION nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+import torch
+
+
+def generate_anchors(stride, ratio_vals, scales_vals, angles_vals=None):
+    "Generate anchors coordinates from scales/ratios"
+
+    scales = torch.FloatTensor(scales_vals).repeat(len(ratio_vals), 1)
+    scales = scales.transpose(0, 1).contiguous().view(-1, 1)
+    ratios = torch.FloatTensor(ratio_vals * len(scales_vals))
+
+    wh = torch.FloatTensor([stride]).repeat(len(ratios), 2)
+    ws = torch.sqrt(wh[:, 0] * wh[:, 1] / ratios)
+    dwh = torch.stack([ws, ws * ratios], dim=1)
+    xy1 = 0.5 * (wh - dwh * scales)
+    xy2 = 0.5 * (wh + dwh * scales)
+    return torch.cat([xy1, xy2], dim=1)
+
+
+def delta2box(deltas, anchors, size, stride):
+    "Convert deltas from anchors to boxes"
+
+    anchors_wh = anchors[:, 2:] - anchors[:, :2] + 1
+    ctr = anchors[:, :2] + 0.5 * anchors_wh
+    pred_ctr = deltas[:, :2] * anchors_wh + ctr
+    pred_wh = torch.exp(deltas[:, 2:]) * anchors_wh
+
+    m = torch.zeros([2], device=deltas.device, dtype=deltas.dtype)
+    M = torch.tensor([size], device=deltas.device, dtype=deltas.dtype) * stride - 1
+    clamp = lambda t: torch.max(m, torch.min(t, M))
+    return torch.cat([clamp(pred_ctr - 0.5 * pred_wh), clamp(pred_ctr + 0.5 * pred_wh - 1)], 1)
+
+
+def decode(all_cls_head, all_box_head, stride=1, threshold=0.05, top_n=1000, anchors=None, rotated=False):
+    "Box Decoding and Filtering"
+
+    if rotated:
+        anchors = anchors[0]
+    num_boxes = 4 if not rotated else 6
+
+    device = all_cls_head.device
+    anchors = anchors.to(device).type(all_cls_head.type())
+    num_anchors = anchors.size()[0] if anchors is not None else 1
+    num_classes = all_cls_head.size()[1] // num_anchors
+    height, width = all_cls_head.size()[-2:]
+
+    batch_size = all_cls_head.size()[0]
+    out_scores = torch.zeros((batch_size, top_n), device=device)
+    out_boxes = torch.zeros((batch_size, top_n, num_boxes), device=device)
+    out_classes = torch.zeros((batch_size, top_n), device=device)
+
+    # Per item in batch
+    for batch in range(batch_size):
+        cls_head = all_cls_head[batch, :, :, :].contiguous().view(-1)
+        box_head = all_box_head[batch, :, :, :].contiguous().view(-1, num_boxes)
+
+        # Keep scores over threshold
+        keep = (cls_head >= threshold).nonzero().view(-1)
+        if keep.nelement() == 0:
+            continue
+
+        # Gather top elements
+        scores = torch.index_select(cls_head, 0, keep)
+        scores, indices = torch.topk(scores, min(top_n, keep.size()[0]), dim=0)
+        indices = torch.index_select(keep, 0, indices).view(-1)
+        classes = (indices / width / height) % num_classes
+        classes = classes.type(all_cls_head.type())
+
+        # Infer kept bboxes
+        x = indices % width
+        y = (indices / width) % height
+        a = indices / num_classes / height / width
+        x = x.int()
+        y = y.int()
+        a = a.int()
+        box_head = box_head.view(num_anchors, num_boxes, height, width)
+        boxes = box_head[a, :, y, x]
+
+        if anchors is not None:
+            grid = torch.stack([x, y, x, y], 1).type(all_cls_head.type()) * stride + anchors[a, :]
+            boxes = delta2box(boxes, grid, [width, height], stride)
+
+        out_scores[batch, : scores.size()[0]] = scores
+        out_boxes[batch, : boxes.size()[0], :] = boxes
+        out_classes[batch, : classes.size()[0]] = classes
+
+    return out_scores, out_boxes, out_classes
+
+
+def nms(all_scores, all_boxes, all_classes, nms=0.5, ndetections=100):
+    "Non Maximum Suppression"
+
+    device = all_scores.device
+    batch_size = all_scores.size()[0]
+    out_scores = torch.zeros((batch_size, ndetections), device=device)
+    out_boxes = torch.zeros((batch_size, ndetections, 4), device=device)
+    out_classes = torch.zeros((batch_size, ndetections), device=device)
+
+    # Per item in batch
+    for batch in range(batch_size):
+        # Discard null scores
+        keep = (all_scores[batch, :].view(-1) > 0).nonzero()
+        scores = all_scores[batch, keep].view(-1)
+        boxes = all_boxes[batch, keep, :].view(-1, 4)
+        classes = all_classes[batch, keep].view(-1)
+
+        if scores.nelement() == 0:
+            continue
+
+        # Sort boxes
+        scores, indices = torch.sort(scores, descending=True)
+        boxes, classes = boxes[indices], classes[indices]
+        areas = (boxes[:, 2] - boxes[:, 0] + 1) * (boxes[:, 3] - boxes[:, 1] + 1).view(-1)
+        keep = torch.ones(scores.nelement(), device=device, dtype=torch.uint8).view(-1)
+
+        for i in range(ndetections):
+            if i >= keep.nonzero().nelement() or i >= scores.nelement():
+                i -= 1
+                break
+
+            # Find overlapping boxes with lower score
+            xy1 = torch.max(boxes[:, :2], boxes[i, :2])
+            xy2 = torch.min(boxes[:, 2:], boxes[i, 2:])
+            inter = torch.prod((xy2 - xy1 + 1).clamp(0), 1)
+            criterion = (scores > scores[i]) | (inter / (areas + areas[i] - inter) <= nms) | (classes != classes[i])
+            criterion[i] = 1
+
+            # Only keep relevant boxes
+            scores = scores[criterion.nonzero()].view(-1)
+            boxes = boxes[criterion.nonzero(), :].view(-1, 4)
+            classes = classes[criterion.nonzero()].view(-1)
+            areas = areas[criterion.nonzero()].view(-1)
+            keep[(~criterion).nonzero()] = 0
+
+        out_scores[batch, : i + 1] = scores[: i + 1]
+        out_boxes[batch, : i + 1, :] = boxes[: i + 1, :]
+        out_classes[batch, : i + 1] = classes[: i + 1]
+
+    return out_scores, out_boxes, out_classes
+
+
+def detection_postprocess(image, cls_heads, box_heads):
+    # Inference post-processing
+    anchors = {}
+    decoded = []
+
+    for cls_head, box_head in zip(cls_heads, box_heads):
+        # Generate level's anchors
+        stride = image.shape[-1] // cls_head.shape[-1]
+        if stride not in anchors:
+            anchors[stride] = generate_anchors(stride, ratio_vals=[1.0, 2.0, 0.5], scales_vals=[4 * 2 ** (i / 3) for i in range(3)])
+        # Decode and filter boxes
+        decoded.append(decode(cls_head, box_head, stride, threshold=0.5, top_n=1000, anchors=anchors[stride]))
+
+    # Perform non-maximum suppression
+    decoded = [torch.cat(tensors, 1) for tensors in zip(*decoded)]
+    # NMS threshold = 0.5
+    scores, boxes, labels = nms(*decoded, nms=0.5, ndetections=100)
+    return scores, boxes, labels
