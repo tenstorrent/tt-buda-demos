3227e5410035e88871049ba554684733f227cb96
kkannan <kkannan@tenstorrent.com>
2024-05-20 17:28:49 +0000

    Add tests for retinanet(pytorch)

---
+++ model_demos/tests/test_pytorch_retinanet.py
@@ -0,0 +1,10 @@
+import pytest
+from cv_demos.retinanet.pytorch_retinanet import run_retinanet_pytorch
+
+variants = ["retinanet_rn18fpn", "retinanet_rn34fpn", "retinanet_rn50fpn", "retinanet_rn101fpn", "retinanet_rn152fpn"]
+
+
+@pytest.mark.retinanet
+@pytest.mark.parametrize("variant", variants)
+def test_retinanet_pytorch(variant, clear_pybuda):
+    run_retinanet_pytorch(variant)
