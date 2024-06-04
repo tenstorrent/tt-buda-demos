de3fb0d492b5e6456d91421649cc733199ad3315
Milan Kordic <milan.kordic94@gmail.com>
2024-06-04 19:46:50 +0000

    Separate PerceiverIO demos by type into standalone scripts

---
+++ model_demos/cv_demos/perceiverio/pytorch_perceiverio_fourier.py
@@ -0,0 +1,52 @@
+# Perceiver IO Fourier Demo Script
+
+import os
+
+import pybuda
+import requests
+from PIL import Image
+from transformers import AutoImageProcessor, PerceiverForImageClassificationFourier
+
+
+def run_perceiverio_fourier_pytorch():
+
+    # Load feature extractor and model checkpoint from HuggingFace
+    model_ckpt = "deepmind/vision-perceiver-fourier"
+    image_processor = AutoImageProcessor.from_pretrained(model_ckpt)
+    model = PerceiverForImageClassificationFourier.from_pretrained(model_ckpt)
+    model.eval()
+
+    # Set PyBuda configuration parameters
+    compiler_cfg = pybuda.config._get_global_compiler_config()
+    compiler_cfg.balancer_policy = "Ribbon"
+    compiler_cfg.default_df_override = pybuda.DataFormat.Float16_b
+    os.environ["PYBUDA_RIBBON2"] = "1"
+    compiler_cfg.enable_auto_fusing = False
+
+    os.environ["PYBUDA_DISABLE_PADDING_PASS"] = "1"
+    os.environ["TT_BACKEND_OVERLAY_MAX_EXTRA_BLOB_SIZE"] = f"{101*1024}"
+
+    # Load data sample
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
+    label = "tabby, tabby cat"
+
+    # Data preprocessing
+    inputs = image_processor(image, return_tensors="pt")
+    pixel_values = inputs["pixel_values"]
+
+    # Run inference on Tenstorrent device
+    output_q = pybuda.run_inference(
+        pybuda.PyTorchModule("pt_" + str(model_ckpt.split("/")[-1].replace("-", "_")), model), inputs=[(pixel_values,)]
+    )
+    output = output_q.get()  # return last queue object
+
+    # Data postprocessing
+    predicted_value = output[0].value().argmax(-1).item()
+    predicted_label = model.config.id2label[predicted_value]
+
+    print(f"True Label: {label} | Predicted Label: {predicted_label}")
+
+
+if __name__ == "__main__":
+    run_perceiverio_fourier_pytorch()
--- model_demos/tests/test_pytorch_perceiverio.py
+++ model_demos/tests/test_pytorch_perceiverio.py
@@ -1,15 +1,20 @@
 import pytest
 
-from cv_demos.perceiverio.pytorch_perceiverio import run_perceiverio_pytorch
-
-variants = [
-    "deepmind/vision-perceiver-conv",
-    "deepmind/vision-perceiver-fourier",
-    "deepmind/vision-perceiver-learned",
-]
+from model_demos.cv_demos.perceiverio.pytorch_perceiverio_conv import run_perceiverio_conv_pytorch
+from model_demos.cv_demos.perceiverio.pytorch_perceiverio_fourier import run_perceiverio_fourier_pytorch
+from model_demos.cv_demos.perceiverio.pytorch_perceiverio_learned import run_perceiverio_learned_pytorch
 
 
-@pytest.mark.parametrize("variant", variants, ids=variants)
 @pytest.mark.perceiverio
-def test_perceiverio_pytorch(clear_pybuda, variant):
-    run_perceiverio_pytorch(variant)
+def test_perceiverio_conv_pytorch(clear_pybuda):
+    run_perceiverio_conv_pytorch()
+
+
+@pytest.mark.perceiverio
+def test_perceiverio_fourier_pytorch(clear_pybuda):
+    run_perceiverio_fourier_pytorch()
+
+
+@pytest.mark.perceiverio
+def test_perceiverio_learned_pytorch(clear_pybuda):
+    run_perceiverio_learned_pytorch()
