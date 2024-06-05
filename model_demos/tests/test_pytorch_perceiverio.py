376c1a6027faf12af816610e9cba937d122796cd
anirudTT <aramchandran@tenstorrent.com>
2024-06-05 19:00:33 +0000

    fix PerceiverIO demos imports

--- model_demos/tests/test_pytorch_perceiverio.py
+++ model_demos/tests/test_pytorch_perceiverio.py
@@ -1,8 +1,8 @@
 import pytest
 
-from model_demos.cv_demos.perceiverio.pytorch_perceiverio_conv import run_perceiverio_conv_pytorch
-from model_demos.cv_demos.perceiverio.pytorch_perceiverio_fourier import run_perceiverio_fourier_pytorch
-from model_demos.cv_demos.perceiverio.pytorch_perceiverio_learned import run_perceiverio_learned_pytorch
+from cv_demos.perceiverio.pytorch_perceiverio_conv import run_perceiverio_conv_pytorch 
+from cv_demos.perceiverio.pytorch_perceiverio_fourier import run_perceiverio_fourier_pytorch
+from cv_demos.perceiverio.pytorch_perceiverio_learned import run_perceiverio_learned_pytorch
 
 
 @pytest.mark.perceiverio
