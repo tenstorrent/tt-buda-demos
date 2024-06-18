c4584a5f6009108f0691797c502b44e47f65603d
pchandrasekaran <160489301+chandrasekaranpradeep@users.noreply.github.com>
2024-06-18 14:06:20 +0530

    Patch Customer demos Weekly n300 ci failures (#105)

--- model_demos/cv_demos/ssd300_resnet50/pytorch_ssd300_resnet50.py
+++ model_demos/cv_demos/ssd300_resnet50/pytorch_ssd300_resnet50.py
@@ -64,7 +64,6 @@ def run_pytorch_ssd300_resnet50():
     compiler_cfg = pybuda.config._get_global_compiler_config()
     compiler_cfg.balancer_policy = "Ribbon"
     compiler_cfg.default_df_override = pybuda.DataFormat.Float16_b
-    compiler_cfg.balancer_op_override("max_pool2d_14.dc.sparse_matmul.5.dc.sparse_matmul.1.lc2", "t_stream_shape", (2, 1))
 
     # Device specific configurations
     available_devices = pybuda.detect_available_devices()
@@ -73,10 +72,11 @@ def run_pytorch_ssd300_resnet50():
             os.environ["PYBUDA_RIBBON2"] = "1"
             compiler_cfg.place_on_new_epoch("reshape_769.dc.matmul.13")
             compiler_cfg.place_on_new_epoch("reshape_769.dc.matmul.7")
+            compiler_cfg.balancer_op_override("max_pool2d_14.dc.sparse_matmul.5.dc.sparse_matmul.1.lc2", "t_stream_shape", (2, 1))
 
         if available_devices[0] == BackendDevice.Wormhole_B0:
             compiler_cfg.place_on_new_epoch("conv2d_766.dc.matmul.11")
-            os.environ["TT_BACKEND_OVERLAY_MAX_EXTRA_BLOB_SIZE"] = "12288"
+            os.environ["TT_BACKEND_OVERLAY_MAX_EXTRA_BLOB_SIZE"] = "36864"
 
     # STEP 2 : prepare model
     model = torch.hub.load("NVIDIA/DeepLearningExamples:torchhub", "nvidia_ssd", pretrained=False)
