a91404adc71b1d701c679317e4334508d68b6a0b
Jackson Nie <jnie@tenstorrent.com>
2024-06-26 06:35:37 -0700

    Updates to run resnet multi chip multi card

--- model_demos/cv_demos/resnet/pytorch_resnet.py
+++ model_demos/cv_demos/resnet/pytorch_resnet.py
@@ -4,16 +4,50 @@
 # ResNet Demo Script
 
 import os
-
+from typing import Optional, List
 import pybuda
 import requests
 from PIL import Image
 from transformers import AutoFeatureExtractor, ResNetForImageClassification
+from pybuda.tensor import Tensor
+from utils.config import Config, DeviceType
+from pybuda.tools.tti_data_parallel import (
+    RunMode,
+    RunResult,
+    ForwardRunInputs,
+    run_tti_data_parallel,
+)
 
-from utils.config import Config
+def run_resnet_single_card(model, pixel_values):
+    # Run inference on Tenstorrent device
+    output_q = pybuda.run_inference(pybuda.PyTorchModule("pt_resnet50", model), inputs=[(pixel_values,)])
+    output = output_q.get()  # return last queue object
+    return output
+
+def run_resnet_multi_card(pixel_values, precompiled_tti_path: str, device_overrides: List[List[int]], output_dir: Optional[str]=None):
+    os.environ["PYBUDA_FORCE_THREADS"] = "1"
+    
+    if not output_dir:
+        output_dir = "./cv_demos/resnet/resnet_multi_card"
+        
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        
+    result: RunResult = run_tti_data_parallel(
+        arch=pybuda.BackendDevice.Wormhole_B0,
+        device_ids=device_overrides,
+        run_mode=RunMode.FORWARD,
+        inputs=ForwardRunInputs(inputs=[pixel_values]), 
+        output_dir=output_dir, 
+        num_loops=1,
+        precompiled_tti_path=precompiled_tti_path,
+        sync_at_run_start=True,
+    )
+    
+    return [Tensor.create_from_torch(output_tensor) for output_tensor in result.outputs[0]]
 
 
-def run_resnet_pytorch(variant="microsoft/resnet-50", config=Config(device="e150", chip_mode="single", batch_size=1)):
+def run_resnet_pytorch(variant="microsoft/resnet-50", config=Config(device=DeviceType.e150, multi_chip=False, batch_size=1)):
 
     # Load ResNet feature extractor and model checkpoint from HuggingFace
     model_ckpt = variant
@@ -28,7 +62,7 @@ def run_resnet_pytorch(variant="microsoft/resnet-50", config=Config(device="e150
     os.environ["PYBUDA_PAD_OUTPUT_BUFFER"] = "1"
 
     # Check for dual-chip mode
-    if config.device == "n300" and config.chip_mode == "dual":
+    if config.n300_data_parallel():
         os.environ["PYBUDA_N300_DATA_PARALLEL"] = "1"
 
     # Load data sample
@@ -43,9 +77,11 @@ def run_resnet_pytorch(variant="microsoft/resnet-50", config=Config(device="e150
     inputs = feature_extractor(image, return_tensors="pt")
     pixel_values = inputs["pixel_values"]
 
-    # Run inference on Tenstorrent device
-    output_q = pybuda.run_inference(pybuda.PyTorchModule("pt_resnet50", model), inputs=[(pixel_values,)])
-    output = output_q.get()  # return last queue object
+    if config.multi_card_dp():
+        output = run_resnet_multi_card(pixel_values, config.precompiled_tti_path, config.multi_card_devices)
+    
+    else:
+        output = run_resnet_single_card(model, pixel_values)
 
     # Data postprocessing
     predicted_value = output[0].value().argmax(-1)
@@ -56,4 +92,12 @@ def run_resnet_pytorch(variant="microsoft/resnet-50", config=Config(device="e150
 
 
 if __name__ == "__main__":
-    run_resnet_pytorch()
+    # run_resnet_pytorch()
+    multi_card_config = Config(
+        device=DeviceType.n300,
+        multi_chip=True,
+        batch_size=256 * 4,
+        precompiled_tti_path="./cv_demos/resnet/resnet_50_dp_new.tti",
+        multi_card_devices=[[i, i + 4] for i in range(4)]
+    )
+    run_resnet_pytorch(config=multi_card_config)
--- model_demos/utils/config.py
+++ model_demos/utils/config.py
@@ -1,31 +1,48 @@
 # utils/config.py
+from dataclasses import dataclass
+from typing import Optional, List
+from enum import Enum
 
+class DeviceType(Enum):
+    e150 = "e150"
+    n150 = "n150"
+    n300 = "n300"
 
+@dataclass
 class Config:
     """
     Configuration class for setting up the device and other parameters.
 
     Attributes:
-        device (str): The device to be used. Options are 'e75', 'e150', 'n150', 'n300'.
-        chip_mode (str): The chip mode to be used. Options are 'single', 'dual'.
-        batch_size (int): The batch size for processing. Must be greater than 1 for 'n300'.
+        device (DeviceType): The device type to run the model on.
+        multi_chip (bool): Whether to run the model in dual-chip mode.
+        batch_size (int): The batch size for input data.
+        multi_card_devices (Optional[List[List[int]]]): The devices to run the model on in multi-card mode.
 
     Raises:
         AssertionError: If 'n300' is selected but batch_size <= 1.
-        AssertionError: If chip_mode is not 'single' or 'dual'.
-        AssertionError: If 'dual' is selected but device is not 'n300'.
+        AssertionError: If 'multi_chip' or 'multi_card' is selected but device is not 'n300'.
     """
-
-    def __init__(self, device, chip_mode="single", batch_size=1):
-        self.device = device
-        self.chip_mode = chip_mode
-        self.batch_size = batch_size
-
-        # Validate chip_mode
-        assert self.chip_mode in ["single", "dual"], "chip_mode must be either 'single' or 'dual'."
-
+    device: DeviceType = DeviceType.e150
+    multi_chip: bool = False
+    batch_size: int = 1
+    
+    # Multi card dp vars
+    precompiled_tti_path: Optional[str] = None
+    multi_card_devices: Optional[List[List[int]]] = None
+    
+    def __post_init__(self):
         # Validate batch_size and chip_mode for n300
-        if self.device == "n300":
-            assert self.batch_size > 1, "For n300 device, batch_size must be greater than 1."
+        if self.device == DeviceType.n300:
+            assert self.batch_size > 1 and self.batch_size % 2 == 0, "For n300 device, batch_size must be greater than 1."
         else:
-            assert self.chip_mode == "single", "Only the n300 device supports 'dual' chip_mode."
+            assert not self.multi_chip and not self.multi_card_devices, "Only the n300 device supports 'dual' chip_mode."
+            
+        if self.multi_card_devices:
+            assert self.batch_size > 1 and self.batch_size % len(self.multi_card_devices) == 0, "For multi-card mode, batch_size must be divisible by the number of cards."
+            
+    def n300_data_parallel(self) -> bool:
+        return self.device == DeviceType.n300 and self.multi_chip
+    
+    def multi_card_dp(self) -> bool:
+        return bool(self.multi_card_devices)
