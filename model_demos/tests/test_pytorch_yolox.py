6051806e0719586fa11bdcb3d7c111737094aaea
kamalrajkannan78 <157608228+kamalrajkannan78@users.noreply.github.com>
2024-07-05 13:29:43 +0530

    Add demo script for YOLOX(n300 & n150)- Pytorch (#110)

---
+++ model_demos/tests/test_pytorch_yolox.py
@@ -0,0 +1,10 @@
+import pytest
+from cv_demos.yolo_x.pytorch_yolox import run_yolox_pytorch
+
+variants = ["yolox_nano", "yolox_tiny", "yolox_s", "yolox_m", "yolox_l", "yolox_darknet", "yolox_x"]
+
+
+@pytest.mark.parametrize("variant", variants)
+@pytest.mark.yolox
+def test_yolox_pytorch(variant, clear_pybuda, test_device):
+    run_yolox_pytorch(variant)
