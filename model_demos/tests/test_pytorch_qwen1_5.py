d1881b167c1ff3c49cf8f5cd57f3f5b05d45cbef
Jush (yupiop12) <36951064+JushBJJ@users.noreply.github.com>
2024-09-03 22:00:58 +1000

    [BOUNTY] Add Qwen1.5 0.5B (#37)
    * Qwen1.5 0.5B pybuda implementation
    
    * remove unneeded requirement
    
    * Update env vars and compiler configs
    
    * remove undefined device_map
    
    * Remove misleading and unnecessary environment variables
    
    * Refine qwen solution
    
    * Rename qwen file
    
    * Rename qwen filename and added qwen1.5-chat
    
    * Add qwen1.5 test case
    
    * Fix typo in pyproject.toml
    
    * Disable dynamic caching
    
    * Add extra whitespace below model title commment
    
    * Fix typo "moadl" to "model"

---
+++ model_demos/tests/test_pytorch_qwen1_5.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+
+from nlp_demos.qwen1_5.pytorch_qwen1_5_chat import run_qwen1_5_chat
+from nlp_demos.qwen1_5.pytorch_qwen1_5 import run_qwen1_5_causal_lm
+
+@pytest.mark.qwen1_5
+def test_qwen1_5_causal_lm_pytorch(clear_pybuda, test_device, batch_size):
+    run_qwen1_5_causal_lm(batch_size=batch_size)
+
+
+@pytest.mark.qwen1_5
+def test_qwen1_5_chat_pytorch(clear_pybuda, test_device, batch_size):
+    run_qwen1_5_chat(batch_size=batch_size)
