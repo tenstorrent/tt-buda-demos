6309fd10ca52a918bf209e8a9a20675fa07e61ac
Milan Kordic <milan.kordic94@gmail.com>
2024-09-12 14:51:59 +0000

    Update batching for YOLOX

--- model_demos/tests/test_pytorch_yolox.py
+++ model_demos/tests/test_pytorch_yolox.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
 
 from cv_demos.yolo_x.pytorch_yolox import run_yolox_pytorch
@@ -5,7 +8,7 @@ from cv_demos.yolo_x.pytorch_yolox import run_yolox_pytorch
 variants = ["yolox_nano", "yolox_tiny", "yolox_s", "yolox_m", "yolox_l", "yolox_darknet", "yolox_x"]
 
 
-@pytest.mark.parametrize("variant", variants)
+@pytest.mark.parametrize("variant", variants, ids=variants)
 @pytest.mark.yolox
-def test_yolox_pytorch(variant, clear_pybuda, test_device):
-    run_yolox_pytorch(variant)
+def test_yolox_pytorch(clear_pybuda, test_device, variant, batch_size):
+    run_yolox_pytorch(variant, batch_size=batch_size)
